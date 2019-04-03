42935bd3d7c9573028cc9343a00a9b3adcc469f5
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-02 22:50:01 -0600

    Adds tokens to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_024804) do
+ActiveRecord::Schema.define(version: 2019_04_03_042608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_04_03_024804) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "access_token"
+    t.string "refresh_token"
   end
 
 end
