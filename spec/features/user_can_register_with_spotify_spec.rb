1dd15e46d570076b3310747ba0c64ca5b122ebdb
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-02 23:05:52 -0600

    User can register test

--- spec/features/user_can_register_with_spotify_spec.rb
+++ spec/features/user_can_register_with_spotify_spec.rb
@@ -14,13 +14,37 @@ RSpec.describe 'As a visitor' do
           expect(current_path).to eq('/auth/spotify')
         end
         describe 'When I sign in successfully on Spotify' do
+          before :each do
+            Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:spotify]
+          end
+
           it 'should redirect me to the dashboard' do
+            visit '/'
+            click_button "Register with Spotify"
+            expect(current_path).to eq('/')
           end
 
           it 'the navigation bar should now include Logout' do
+            visit '/'
+            click_button "Register with Spotify"
+            expect(page).to have_content("Logout")
+            expect(page).to_not have_content("Login")
           end
 
           it 'I get a flash message that I successfully created an account' do
+            visit '/'
+            click_button "Register with Spotify"
+            expect(page).to have_content("Logout")
+            expect(page).to_not have_content("Login")
+
+            expect(page).to have_content("You have successfully registered an account")
+          end
+
+          it 'I should now have an account in the system' do
+            visit '/'
+            click_button "Register with Spotify"
+            expect(User.count).to eq(1)
+            expect(User.last.name).to eq('Fake Name')
           end
 
 
