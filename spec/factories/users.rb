f0d20938bcd35852a1f4364f6d2bc88e0327d458
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-02 20:57:28 -0600

    User model with test

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    spotify_id { "MyString" }
+    name { "MyString" }
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:spotify_id) }
+    it { should validate_presence_of(:name) }
+
+    it {should validate_uniqueness_of(:spotify_id)}
+
+    it {should validate_length_of(:name)
+      .is_at_least(1)}
+  end
+end
