6fbd5d3917979e4a75aa159d1b71cff704699683
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-02 19:31:36 -0600

    Change test expectation

--- spec/features/visitor/visits_homepage_spec.rb
+++ spec/features/visitor/visits_homepage_spec.rb
@@ -17,8 +17,8 @@ feature 'as a visitor' do
     end
 
     it 'sees an about section' do
-      expect(page).to have_css('.about')
-      within '.about' do
+      expect(page).to have_css('#about')
+      within '#about' do
         expect(page).to have_content(
           'At parties, there is often one person who ends up being in charge of the music. What if everyone’s preferences could be taken into consideration by simply inviting one’s friends via a text message, from there, the music would then take care of itself. DJ Spoti aggregates the favorite songs of all session party users and cultivates a playlist with all user preferences in mind.'
         )
