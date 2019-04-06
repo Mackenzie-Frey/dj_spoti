a5dbcabbeb4697175a455bd91070b7e3eeafbd29
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-06 14:33:26 -0600

    Begins on test for party_playlist recommendation.

--- spec/services/spotify_service_spec.rb
+++ spec/services/spotify_service_spec.rb
@@ -35,6 +35,16 @@ describe 'SpotifyClient' do
       current_song = @service.currently_playing
       expect(current_song).to eq(json)
     end
+
+# for current user
+# get song_ids from a party, for each user
+# get the array from the party, no matter how long and take top five? or random five?
+
+    it '#party_playlist' do
+      json = @service.json_for('me/player/currently-playing')
+      ['3LjhVl7GzYsza1biQjTpaN', '3JhNCzhSMTxs9WLGJJxWOY', '6beUvFUlKliUYJdLOXNj9C',
+        '5BcAKTbp20cv7tC5VqPFoC', '1vCWHaC5f2uS3yhpwWbIA6']
+    end
   end
 
 end
