ea41bd6b89efb1c6d6b31be463df094eae6e78a0
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-07 14:20:35 -0600

    Adds methods for update_playlist_seeds & create.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,6 +1,13 @@
 class Playlist
   def initialize(party_users)
-    binding.pry
     @party_users = party_users
+  end
+
+  def make_playlist_seeds
+    SpotifyService.new(token).top_plays
+  end
+
+  def update_playlist_seeds
+
   end
 end
