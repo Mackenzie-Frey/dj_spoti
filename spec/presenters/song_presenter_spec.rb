fb87b2da9fb9f63b997569ac659d6f4ce6342af3
manojpanta <manojpanta95@gmail.com>
2019-04-09 17:41:16 -0600

    removes expires column from users table thats why

--- spec/presenters/song_presenter_spec.rb
+++ spec/presenters/song_presenter_spec.rb
@@ -7,9 +7,8 @@ describe 'SongFacade' do
                        created_at: Time.now,
                        updated_at: Time.now - 3200,
                        access_token: '',
-                       refresh_token: "AQCzIKW8-VCySiDZQ-OIn8LZHWPCG-TbBhxIHdgPkp738tNEtBtyV8YJ-ccTkhua-80oF1AI4GOwNm4kIdhnwdR7NY6o99y_bBSSWBf_-_HfnP5xNZQb9dt6jYLNE71Qxm7wow",
-                       expires: true,
-                       expires_at: 1554837955)
+                       expires_at: Time.now,
+                       refresh_token: "AQCzIKW8-VCySiDZQ-OIn8LZHWPCG-TbBhxIHdgPkp738tNEtBtyV8YJ-ccTkhua-80oF1AI4GOwNm4kIdhnwdR7NY6o99y_bBSSWBf_-_HfnP5xNZQb9dt6jYLNE71Qxm7wow")
     @facade = SongFacade.new(user)
     @song_json = File.open('./fixtures/example_current_song.json')
     stub_request(:get, 'https://api.spotify.com/v1/me/player/currently-playing')
