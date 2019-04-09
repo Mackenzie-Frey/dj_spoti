ab65b17f75adea04e0898eca7c93fa6bd46a1c10
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-09 11:13:48 -0600

    Adds next step comments and refactors Spotify service call to it's own method.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -6,9 +6,8 @@ class Playlist
 
   def aggregated_top_play_ids
     all_ids = @party_users.map do |party_user|
-      # memoize the below line?
-      # when to invalidate this cache, every couple days?
-      SpotifyService.new(party_user.access_token).top_plays
+      binding.pry
+      user_top_plays(party_user)
     end
      @party.playlist_seeds = select_seeds(all_ids)
      @party.save!
@@ -17,4 +16,19 @@ class Playlist
   def select_seeds(all_ids)
     all_ids.flatten.uniq.sample(5).join(',')
   end
+
+# might need to be adjusted after saving seed artists to users db
+  def user_top_plays(party_user)
+    SpotifyService.new(party_user.access_token).top_plays
+  end
 end
+
+# time = Benchmark.measure do
+#   SpotifyService.new(party_user.access_token).top_plays
+# end
+
+# create a migration to add seed_artists to a users
+
+# Manually store the value for the api call by running an if statement
+
+# rake task to update it the values.
