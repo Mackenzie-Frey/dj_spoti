90959fe25a46d821f80c36db80522f7ca900211c
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-08 20:26:00 -0600

    Removes the use of an environment variable, as the request is stubbed.

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -7,16 +7,9 @@ context 'The aggregated party playlist & seeds' do
     @party = create(:party, admin: @user)
     stub_spotify_top_plays
 
-    # test in the party for a playlist being created
-    # these users all have the same key which expires every hour
-    # this test setup will need to be re-done utilizing the refresh_token,
-    # once that functionality is integrated.
-    # Make a test that when a user logs in and hits create a party,
-    # that they have a playlist for that new party.
-
-    @party.users <<  create(:user, spotify_id: 2, access_token: ENV['HOURLY_SPOTIFY_TOKEN'])
-    @party.users << create(:user, spotify_id: 3, access_token: ENV['HOURLY_SPOTIFY_TOKEN'])
-    @party.users << create(:user, spotify_id: 4, access_token: ENV['HOURLY_SPOTIFY_TOKEN'])
+    @party.users <<  create(:user, spotify_id: 2, access_token: 'fake_token_request_is_stubbed')
+    @party.users << create(:user, spotify_id: 3, access_token: 'fake_token_request_is_stubbed')
+    @party.users << create(:user, spotify_id: 4, access_token: 'fake_token_request_is_stubbed')
 
     @playlist = Playlist.new(@party)
   end
@@ -34,15 +27,9 @@ context 'The aggregated party playlist & seeds' do
   end
 
   it '#update - updates when a non-admin user joins a party' do
-    new_user = create(:user, name: 'new party animal', spotify_id: 5, access_token: ENV['HOURLY_SPOTIFY_TOKEN'])
+    new_user = create(:user, name: 'new party animal', spotify_id: 5, access_token: 'fake_token_request_is_stubbed')
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(new_user)
     @party.users << new_user
-
-    # run coverage to see what is not tested
-    # add a user to a party to call the update method
-    # or add to test for a user joining a party that it changes the playlist_seeds
-    # should any vcr's be used
-    # are the environment variables accounted for? ie HOURLY_SPOTIFY_TOKEN
 
     @playlist.aggregated_top_play_ids
     initial_playlist_seeds = @party.playlist_seeds
