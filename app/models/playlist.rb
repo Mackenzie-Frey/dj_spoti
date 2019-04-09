602c6f9fe9f55b9b1119fdff51fb011f35438e86
Mackenzie-Frey <mackenziefrey1@gmail.com>
2019-04-09 12:26:52 -0600

    Playlist seeds are saving to the party.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -4,12 +4,11 @@ class Playlist
   end
 
   def aggregated_top_play_ids
-    all_ids = party.users.map do |party_user|
-      party_user.seed_artists
+    all_ids = @party.users.map do |party_user|
       user_top_plays(party_user)
     end
-     @party.playlist_seeds = select_seeds(all_ids)
-     @party.save!
+   @party.playlist_seeds = select_seeds(all_ids)
+   @party.save!
   end
 
   def select_seeds(all_ids)
@@ -17,18 +16,23 @@ class Playlist
   end
 
   def user_top_plays(party_user)
+    #change this to if! then do the bottom statement
     if party_user.seed_artists
       party_user.seed_artists
     else
       party_user.update!(seed_artists:
       SpotifyService.new(party_user.access_token).top_plays)
     end
+    party_user.seed_artists
   end
 end
 
 # time = Benchmark.measure do
 #   SpotifyService.new(party_user.access_token).top_plays
 # end
+
+# change the datatype on parties table for playlist_list seeds to an array
+# Sanitize the array before it is saved as party_user.update!
 
 # rake task to update it the values.
 
