19e137c62d74bea30e4619e07c495373c3c388b3
Peregrine Balas <peregrinereedbalas@gmail.com>
2019-04-09 18:46:36 -0600

    Updates to models

--- app/models/party_user.rb
+++
@@ -1,4 +0,0 @@
-class PartyUser < ApplicationRecord
-  belongs_to :party
-  belongs_to :user
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,5 @@ class User < ApplicationRecord
   validates :spotify_id, presence: true,
             uniqueness: { message: "This Spotify account has already been registered with DJ Spoti"}
   validates :name, presence: true, length: { minimum: 1 }
-  has_many :party_users
-  has_many :parties, through: :party_users
+  has_one :party
 end
