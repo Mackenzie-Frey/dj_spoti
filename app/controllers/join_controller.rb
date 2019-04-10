793846d3c734664fd0dced21ffc682608a7bac11
manojpanta <manojpanta95@gmail.com>
2019-04-09 19:26:24 -0600

    vcr

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -1,6 +1,11 @@
 class JoinController < ApplicationController
   def show
     if session[:party_identifier]
+      if !Party.find_by(identifier: session[:party_identifier])
+        session[:party_identifier] = nil
+        redirect_to spotify_omniauth_path(url: params[:i])
+      else
+      end
     else
       redirect_to spotify_omniauth_path(url: params[:i])
     end
