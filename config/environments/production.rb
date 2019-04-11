2d033bfde71efee7b38a8817a17c5c08294c2a3a
Teresa Knowles <teresa07moreno@gmail.com>
2019-04-11 01:49:38 -0600

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_cable.url = "ws://dj-spoti.herokuapp.com/cable"
+  config.action_cable.url = "wss://dj-spoti.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://dj-spoti.herokuapp.com', 'http://dj-spoti.herokuapp.com']
   # Code is not reloaded between requests.
   config.cache_classes = true
